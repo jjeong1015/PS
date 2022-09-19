236d77cd5b3259270ef99896a5cc54aaff4a140d
이정민 <82391356+jjeong1015@users.noreply.github.com>
2022-09-19 10:53:53 +0900

    [Bronze V] Title: A+B - 2, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static StringBuilder sb;
+    static int A, B;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        logic();
+    }
+    static void input() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        A = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        B = Integer.parseInt(st.nextToken());
+    }
+    static void logic() {
+        sb.append(A+B);
+        System.out.println(sb);
+    }
+}
