b19445157e700631a668077f1d5221be067e3206
JJEONG <82391356+jjeong1015@users.noreply.github.com>
2022-09-25 15:16:14 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br;
+    static StringBuilder sb;
+    static StringTokenizer st;
+    static long A, B, C;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        logic();
+    }
+
+    static void input() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        A = Long.parseLong(st.nextToken());
+        B = Long.parseLong(st.nextToken());
+        C = Long.parseLong(st.nextToken());
+    }
+
+    static void logic() {
+        sb.append(A+B+C);
+        System.out.println(sb);
+    }
+}
