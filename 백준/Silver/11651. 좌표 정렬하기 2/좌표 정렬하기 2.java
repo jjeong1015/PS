d5f0d261d6c23f8175b72b109cb6947daedd9a87
JJEONG <82391356+jjeong1015@users.noreply.github.com>
2022-10-01 20:31:15 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 744 ms, Memory: 50128 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br;
+    static StringBuilder sb;
+    static StringTokenizer st;
+    static int N, X, Y;
+    static XY coordinate[];
+    public static void main(String[] args) throws IOException {
+        input();
+        logic();
+    }
+
+    static class XY implements Comparable<XY> {
+        int X, Y;
+        public XY(int X, int Y) {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public int compareTo(XY other) {
+            if ((other.Y == this.Y)) {
+                return Integer.compare(this.X, other.X);
+            }
+            else {
+                return Integer.compare(this.Y, other.Y);
+            }
+        }
+    }
+
+    static void input() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        coordinate = new XY[N];
+        for (int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            X = Integer.parseInt(st.nextToken());
+            Y = Integer.parseInt(st.nextToken());
+            coordinate[i] = new XY(X, Y);
+        }
+    }
+
+    static void logic() {
+        sb = new StringBuilder();
+        Arrays.sort(coordinate);
+        for (int i=0; i<N; i++) {
+            sb.append(coordinate[i].X).append(" ").append(coordinate[i].Y).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
