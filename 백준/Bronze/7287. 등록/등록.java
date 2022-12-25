4c40cb22b712f6e919431f6a040e8b91acbff0ef
JJEONG <82391356+jjeong1015@users.noreply.github.com>
2022-12-25 19:53:27 +0900

    [Bronze V] Title: 등록, Time: 124 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class Main {
+    static BufferedReader br;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        sb.append("145").append("\n");
+        sb.append("shinin2008");
+        System.out.println(sb);
+    }
+}
